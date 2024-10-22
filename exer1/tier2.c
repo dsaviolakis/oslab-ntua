6d2f08939af6f11be0131ac30f2bdc15bc2e1cc2
Dimitrios Saviolakis <dsavi.git@gmail.com>
2024-10-22 19:42:15 +0000

    Solved ch16

--- exer1/tier2.c
+++ exer1/tier2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <sys/mman.h>
 
 int setup_tier2 () {
+	long int *addr = mmap(0x6042000, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	*addr = getpid();
 	return 0;
 }
